1e5a091b63e8f28c5c39968e02d583ed1d76a3f8
Gilbert/Ki-Sung <kcs4912@gmail.com>
2023-05-03 20:28:05 +0900

    day4 start node js - debuging

--- DAY4/gilbert/routes/index.js
+++ DAY4/gilbert/routes/index.js
@@ -4,7 +4,15 @@ var router = express.Router();
 /* GET home page. */
 // http://localhost:300/
 router.get('/', function(req, res, next) {
+  
+  var test = req.query.uid;
+
+  console.log("메인페이지 라우팅 메소드가 호출되었습니다.")
+
+  console.log("view 폴더내에 index.ejs파일 내용(html)이 브라우저로 전달되었습니다.")
+  
   res.render('index', { title: 'Gilbert' });
+
 });
 
 module.exports = router;
