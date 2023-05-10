42b9004eaf373ba67046e05997d78ae2864b7656
Gilbert/Ki-Sung <kcs4912@gmail.com>
2023-05-10 22:06:00 +0900

    structure routing method

--- DAY7/nodeadminapp/routes/article.js
+++ DAY7/nodeadminapp/routes/article.js
@@ -12,12 +12,72 @@ var router = express.Router();
 // localhost:3000/article/list
 router.get('/list', async(req, res) => {
 
-    res.render('article/list.ejs');
+    // 전체 게시글 정보를 DB에서 조회해옴.
+    var articles = [
+        {
+            aid: "1",
+            title: "HelloWord 1",
+            contents: "게시글 내용 1",
+            view_cnt: 40,
+            display_yn: "Y",
+            ip_address: "111.111.111.111",
+            regist_date: Date.now(),
+            regist_user: "Gilbert"
+        },
+        {
+            aid: "2",
+            title: "HelloWord 2",
+            contents: "게시글 내용 2",
+            view_cnt: 20,
+            display_yn: "Y",
+            ip_address: "111.111.111.112",
+            regist_date: Date.now(),
+            regist_user: "Gilbert"
+        },
+        {
+            aid: "3",
+            title: "HelloWord 3",
+            contents: "게시글 내용 3",
+            view_cnt: 0,
+            display_yn: "N",
+            ip_address: "111.111.111.113",
+            regist_date: Date.now(),
+            regist_user: "Gilbert"
+        }
+    ];
+
+    res.render('article/list.ejs', {articles:articles});
 
 });
 
-// 게시글 조회 선택옵션에 따른 게시글 데이터 조회 처리 요청 및 응답 라우팅 메소드
-// router.post();
+// 게시글 조회 선택옵션에 따른 게시글 데이터 조회 처리 요청 및 응답 라우팅 메소드 
+//사용자가 조회 옵션 정보를 입력/선택 후 조회버튼을 클릭하면 전달되는 조회옵션 데이터를 추출해 DB에서 데이터를 조회 후 다시 list.ejs 조회 목록 데이터를 전달함.
+// localhost:3000/article/list
+router.post('/list', async(req, res) => {
+
+    // step 1. 사용자가 입려/선택한 조회옵션 데이터를 추출함.
+    var title = req.body.title;
+    var ipaddress = req.body.ipaddress;
+    var displayyn = req.body.displayyn;
+
+    // step 2. 추출된 조회옵션 데이터기반으로 게시글 테이블에서 해당 게시글 목록을 조회해옴. 
+    var articles = [
+        {
+            aid: "1",
+            title: "HelloWord 1",
+            contents: "게시글 내용 1",
+            view_cnt: 40,
+            ip_address: "111.111.111.111",
+            display_yn: "Y",
+            regist_date: Date.now(),
+            regist_user: "Gilbert"
+        }
+    ];
+
+    // step 3. 조회 결과 목록 데이터를 list.ejs view에 전달함.
+    res.render('article/list', {articles});
+
+});
 
 
 
