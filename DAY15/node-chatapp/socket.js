5d8136ea284b46f0c97c02d8223d4a8a43bac0a9
Gilbert/Ki-Sung <kcs4912@gmail.com>
2023-06-08 20:57:53 +0900

    make chat-gpt chatbot

--- DAY15/node-chatapp/socket.js
+++ DAY15/node-chatapp/socket.js
@@ -1,5 +1,6 @@
 // socket.io 패키지 참조 
 const SoketIO = require("socket.io");
+const { Configuration, OpenAIApi } = require("openai");
 
 // socket.js 모듈 기능 정의 
 module.exports = (server) => {
@@ -17,15 +18,31 @@ module.exports = (server) => {
         });
 
         // socket.on("서버 이벤트 수신기명", 클라이언트에서 서버이벤트로 전달되는 파라미터 처리 콜백함수)
-        socket.on("broadcast", function(msg){
+        socket.on("broadcast", async(message) => {
             
             // io.emit("클라이언트 이벤트 수신기명 정의", 클라이언트 이벤트 수신기에 전달할 데이터)
             // io.emit() 메소드는 서버소켓(socket.js)와 연결된 모든 클라이언트에게 메시지가 발송된다.
             // 예를들면 100명의 사용자가 브라우저(사용자)가 socket.js서버 소켓과 연결이 되어 있다면 100명에 동일한 메세지가 전송됨.
-            io.emit("receiveAll", msg);
-            console.log("클라이언트 receiveAll 이벤트 수신기 호출")
+            // io.emit("receiveAll", msg);
+            // console.log("클라이언트 receiveAll 이벤트 수신기 호출")
             //socket.broadcast.emit("receive",msg);
-        });
+
+            const configuration = new Configuration({
+                apiKey: "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",
+            });
+            
+            const openai = new OpenAIApi(configuration);
+            
+            const response = await openai.createChatCompletion({
+                model: "gpt-3.5-turbo",
+                messages: [{ role: "user", content: message }],
+            });
+
+            var resultMsg = response.data.choices[0].message;
+
+            //현재 접속자에게만 발송
+            socket.emit("receiveAll", `${resultMsg.role}:${resultMsg.content}`);
+            });
 
         // 샘플용 서버 이벤트 수신기 정의 1 - 비동기 방식 
         socket.on("test1", async(data1, data2) => {
